dfdb5bdc0d818f1a97aa917533c59f9899ae4182
Takuya Iwanaga <takuyai@gmail.com>
2025-07-12 16:24:32 +1000

    Add Aqua tests, remove unused dependencies

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,11 @@
 using ReefGuide
 using Test
+using Aqua
+
+@testset "Aqua" begin
+    Aqua.test_undefined_exports(ReefGuide)
+    Aqua.test_stale_deps(ReefGuide; ignore=[:GeoJSON])
+end
 
 @testset "ReefGuide.jl" begin
     # TODO real tests
