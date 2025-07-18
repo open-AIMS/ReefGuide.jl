19da7274f6ac3d306c123ca751ef20365f6c19e1
Takuya Iwanaga <takuyai@gmail.com>
2025-07-18 10:57:58 +1000

    Add additional precompilation work
    Potentially speed up very slow first COG write.

--- src/ReefGuide.jl
+++ src/ReefGuide.jl
@@ -17,7 +17,7 @@ using FLoops, ThreadsX
 
 # Precompilation
 using PrecompileSignatures: @precompile_signatures
-# using PrecompileTools
+using PrecompileTools
 
 # Utilities and helpers for assessments
 include("utility/utility.jl")
@@ -35,5 +35,26 @@ export initialise_data,
 
 # Auto-generate precompilation signatures for ReefGuide
 @precompile_signatures(ReefGuide)
+
+# Force precompilation of methods that slow down initial use.
+@setup_workload begin
+    @compile_workload begin
+        # Enforce precompilation of specific geospatial read/write methods
+        GeoParquet.read(joinpath(pkgdir(ReefGuide), "assets", "dummy.parq"))
+        GDF.read(joinpath(pkgdir(ReefGuide), "assets", "dummy.gpkg"))
+
+        tmpfile = tempname()
+        dims = (X(1.0:1000.0), Y(1:1000.0))
+        x = Raster(rand(1000, 1000); dims=dims)
+        _write_cog(tmpfile, x; tile_size=(256,), num_threads=1)
+
+        x = Raster(sparse(rand(1000, 1000)); dims=dims)
+        _write_cog(tmpfile, x; tile_size=(256,), num_threads=1)
+
+        x2 = ExtendableSparseMatrix{Int8,Int64}(1000, 1000)
+        x = Raster(x2; dims=dims)
+        _write_cog(tmpfile, x; tile_size=(256,), num_threads=1)
+    end
+end
 
 end
